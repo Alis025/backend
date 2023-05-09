01dcf54c09fa1565b137b0e3fafb0ad80c896943
Alis025 <98990150+Alis025@users.noreply.github.com>
2023-05-09 14:52:41 -0600

    Add files via upload

---
+++ src/main/kotlin/flyWeight/Product.kt
@@ -0,0 +1,10 @@
+package flyWeight
+
+class Product(
+    private val brand: String,
+    private val id: Long,
+    private val price: Double,
+    private val descriptionProduct: String)  {
+
+}
+ 
